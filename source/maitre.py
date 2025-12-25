d2df664aca1b21346b1a74af4569460742c76a16
theofefe22 <atlantide.pictures.bzh@gmail.com>
2025-12-25 14:41:26 +0100

    Maj maitre.py

--- source/maitre.py
+++ source/maitre.py
@@ -1,78 +1,225 @@
-import gui7 as gui
+# BIBLIOTHEQUES
 import rotator as rt
-import sys
+import __gui as gui
+import mariadb
 import socket
 import threading
+import sys
 
+
+# CLASSES
 alfa = rt.A64()
+rsa = rt.RSA()
 
+
+# FONCIONS
 def gestion_clients(connexion, adresse):
-    print(f"Client connecté {adresse}")
-    
+    print(f"Client connecté : {adresse}")
+    curseur, connexion_bdd = ouvrir()
+    historique(curseur, f"Connexion d'un appareil {adresse}.")
     buffer = ""
     bloc = []
-    
     try:
         while True:
-            donnes = connexion.recv(1024)
-            if not donnes:
+            donnees = connexion.recv(1024)
+            if not donnees:
                 break
+            buffer += donnees.decode("utf-8")
             
-            buffer += donnes.decode()
+            if "Liste routeurs" in buffer:
+                historique(curseur, f"L'appareil {adresse} a demandé la liste des appareils.")
+                liste_appareils = recuperation(curseur)
+                for appareil in liste_appareils:
+                    connexion.sendall((str(appareil) + "JEPARS").encode("utf-8"))
+                connexion.sendall("FIN-TRANS".encode("utf-8"))
+                connexion.shutdown(socket.SHUT_WR)
+                buffer = buffer.replace("Liste routeurs", "", 1)
             
-            while "JEPARS" in buffer:
-                ligne, buffer = buffer.split("JEPARS", 1)
-                if ligne == "FIN-TRANS":
-                    print(f"Message de {adresse}")
-                    print(bloc)
-                    bloc.clear()
-                else:
-                    bloc.append(ligne)
-    finally:
-        connexion.close()
-
-# Fonction principale d'exécution
-def main():
-    app = gui.QApplication(sys.argv)
-    app.setStyleSheet(gui.qss)
-
-    # RSA
-    rsa = rt.RSA()
-    if not rt.os.path.exists(f"{rt.os.path.splitext(rt.os.path.basename(__file__))[0]}_cle_publique.pem"):
-        rsa.cles(512)
-        rsa.sauvegarde_cles(rt.os.path.splitext(rt.os.path.basename(__file__))[0], 7, alfa.z64_inverse('0zViens78Voirles Mousaillosn!!', 4))
-
-    ma_cle_publique, ma_cle_privee = rsa.chargement_cles(rt.os.path.splitext(rt.os.path.basename(__file__))[0], 7, alfa.z64_inverse('0zViens78Voirles Mousaillosn!!', 4))
-   
+            if "QUIT" in buffer:
+                
+                historique(curseur, f"L'appareil {adresse} de nom {nom} s'est déconnecté.")
+                curseur.execute("DELETE FROM appareils WHERE nom = ?", (nom,))
+                curseur.connection.commit()
+                continue
+            
+            if "Demande initialisation" in buffer:
+                historique(curseur, f"L'appareil {adresse} a demandé à s'initialiser au registre.")
+                connexion.sendall("ACK-INITIALISATION".encode("utf-8"))
+                historique(curseur, "Acceptation d'enregistrement.")
+                buffer = buffer.replace("Demande initialisation", "", 1)
+                param = buffer
+                buffer = ""
+                while "FIN-PARAM" not in param:
+                    seg = connexion.recv(1024)
+                    if not seg:
+                        break
+                    param += seg.decode("utf-8")
+                param = param.replace("FIN-PARAM", "")
+                param = param.split("JEPARS")
+                attributs = {}
+                attributs["nom"] = param[0]
+                attributs["ip"] = param[1]
+                attributs["port"] = param[2]
+                bloc_kp = param[3].strip()
+                bloc_kp = bloc_kp.lstrip("{").rstrip("}")
+                paires = bloc_kp.split(",")
+                for p in paires:
+                    if ":" not in p:
+                        continue
+                    cle, valeur = p.split(":", 1)
+                    cle = cle.strip().strip("'").strip('"')
+                    valeur = valeur.strip().strip("'").strip('"')
+                    attributs[cle] = valeur
+                curseur.execute("SELECT id, nom FROM appareils WHERE n = ? AND e = ?", (attributs['n'], attributs['e']))
+                iport = curseur.fetchone()
+                if iport: # MAJ des clés
+                    nom = iport[1]
+                    curseur.execute("UPDATE appareils SET ip = ?, port = ? WHERE id = ?", (attributs["ip"], attributs["port"], iport[0]))
+                    curseur.connection.commit()
+                else: # Nouvelle entrée
+                    # Changement de nom
+                    if "Client" in attributs["nom"]:
+                        attributs["nom"] = "Client " + str(rt.rds.randint(0, 99999))
+                    else:
+                        attributs["nom"] = "Routeur " + str(rt.rds.randint(0, 99999))
+                    nom = attributs["nom"]
+                    insertion(curseur, attributs)
+                    historique(curseur, f"Insertion de {adresse} et changement de nom en {attributs['nom']}.")
+                connexion.sendall(("NOUVEAU-NOM" + nom).encode("utf-8"))
+                break
+                
+    except ConnectionResetError as cre:
+        historique(curseur, f"Erreur de connexion de {adresse} : {e}.")
     
+    finally:
+        fermer(curseur, connexion_bdd)
+        connexion.close()
+                
+def ecoute(port):
+    serveur = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+    serveur.bind(("0.0.0.0", port))
+    serveur.listen()
+    
+    while True:
+        connexion, adresse = serveur.accept()
+        thread = threading.Thread(
+            target = gestion_clients,
+            args = (connexion, adresse),
+            daemon = True)
+        thread.start()
+
+def ouvrir():
+    try:
+        connexion_bdd = mariadb.connect(
+            user = "master1",
+            password = "JEsuisLEmaitre1",
+            host = "127.0.0.1",
+            database = "rotator",
+            )
+        curseur = connexion_bdd.cursor()
+        print("Connecté à la base de données")
+        return curseur, connexion_bdd
+    except mariadb.Error as e:
+        print(f"Erreur de connexion : {e}")
+        exit()
+
+def fermer(curseur, connexion_bdd):
+    curseur.close()
+    connexion_bdd.close()
+
+def initialisation(curseur):
+    try:
+        curseur.execute(f"CREATE DATABASE IF NOT EXISTS rotator")
+        curseur.execute(f"""CREATE TABLE IF NOT EXISTS appareils (
+            id INT PRIMARY KEY AUTO_INCREMENT,
+            nom VARCHAR(20),
+            ip VARCHAR(15),
+            port VARCHAR(5),
+            n VARCHAR(2200) UNIQUE,
+            e VARCHAR(2200) UNIQUE)""")
+        curseur.execute(f"ALTER TABLE appareils ADD UNIQUE (n)")
+        curseur.execute(f"""CREATE TABLE IF NOT EXISTS appareils_historique (
+            id INT PRIMARY KEY AUTO_INCREMENT,
+            nom VARCHAR(20),
+            ip VARCHAR(15),
+            port VARCHAR(5),
+            n VARCHAR(2200) UNIQUE,
+            e VARCHAR(2200) UNIQUE)""")
+        curseur.execute(f"""CREATE TABLE IF NOT EXISTS historique (
+            id INT PRIMARY KEY AUTO_INCREMENT,
+            date DATETIME NOT NULL,
+            description TEXT NOT NULL)""")
+    except mariadb.Error as e:
+        print(f"Erreur d'initialisation : {e}")
+
+def insertion(curseur, attributs: dict):
+    try:
+        curseur.execute("INSERT INTO appareils (nom, ip, port, n, e) VALUES (?, ?, ?, ?, ?)", (attributs["nom"], attributs["ip"], attributs["port"], attributs["n"], attributs["e"]))
+        curseur.connection.commit()
+        print(f"Ajout de données à la base appareils")
+    except mariadb.Error as e:
+        print(f"Erreur d'ajout : {e}")
+
+def historique(curseur, description: str):
+    try:
+        curseur.execute("INSERT INTO historique (date, description) VALUES (NOW(), ?)", (description,))
+        curseur.connection.commit()
+        print(f"Ajout de données à l'historique")
+    except mariadb.Error as e:
+        print(f"Erreur d'ajout : {e}")
+
+def recuperation(curseur):
+    curseur.execute(f"SELECT id, nom, ip, port, n, e FROM appareils")
+    liste_appareils = []
+    
+    for (id, nom, ip, port, n, e) in curseur:
+        appareil = {'nom': nom, 'ip': ip, 'port': port, 'n': n, 'e': e}
+        liste_appareils.append(appareil)
+    
+    print(liste_appareils)
+    return liste_appareils
+
+
+def main():
+    # GESTION DE MES CLES RSA
+    K_ECDHE = ('0z0 ASDMdcYYcPf0kDSkQ99OrxxeXYblyCPtECL0FmSbh', '0zWZ5RZZ6bFxucTPIb8mvHPOEyYc4CCTbH31B3ergWmJ9')
+
+    if not rt.os.path.exists(f"{rt.os.path.splitext(rt.os.path.basename(__file__))[0]}_cle_publique.pem"):
+        rsa.cles(683)
+        curseur, connexion_bdd = ouvrir()
+        historique(curseur, "Création des clés RSA du Maître.")
+        fermer(curseur, connexion_bdd)
+        print(rsa.Km)
+        print(rsa.Kp)
+        rsa.sauvegarde_cles(rt.os.path.splitext(rt.os.path.basename(__file__))[0], 7, alfa.z64_inverse('0z0 ASDMdcYYcPf0kDSkQ99OrxxeXYblyCPtECL0FmSbh', 7))
+
+    ma_cle_publique, ma_cle_privee = rsa.chargement_cles(rt.os.path.splitext(rt.os.path.basename(__file__))[0], 7, alfa.z64_inverse(K_ECDHE[0], 7))
+
+    print(f"Ma clé publique RSA est : {ma_cle_publique}")
+    print(f"Ma clé privée RSA est : {ma_cle_privee}")
+
     port = rt.rds.randint(2000, 49999)
     
-    liste_appareils = [
-            ["Routeur 1", "465.15.782.16", "65300", "0x46e8765ef17b7fa9"],
-            ["Routeur 2", "465.15.762.97", "65301", "0x17b7fa9"],
-            ["Routeur 5", "465.15.782.13", "65301", "0x17b7fa684346859"],
-            ["Client 456", "465.15.782.13", "65301", "0x17b7fa684346859"]
-        ]
-    
-    
-    
-    # Fenêtre client
-    fenetre_client = gui.Maitre(mon_ip = rt.ip(), mon_port = port, liste_appareils = liste_appareils, ma_cle_publique = ma_cle_publique, ma_cle_privee = ma_cle_privee)
-    fenetre_client.showMaximized()
-    
-    
+    curseur, connexion_bdd = ouvrir()
+    initialisation(curseur)
+    historique(curseur, "Démarrage du Maître.")
+    historique(curseur, f"Maître en écoute sur le port {port} de {rt.ip()}.")
+    fermer(curseur, connexion_bdd)
 
-    # Fermeture application
-    sys.exit(app.exec())
+    threading.Thread(target = ecoute, args = (port,), daemon = True).start()
+
+    app = gui.QApplication(sys.argv)
+    app.setStyleSheet(gui.qss)
     
+    fenetre = gui.Maitre(mon_ip = rt.ip(), mon_port = port, liste_appareils = {}, ma_cle_publique = ma_cle_publique, ma_cle_privee = ma_cle_privee)
+    fenetre.showMaximized()
+    
+    sys.exit(app.exec())
+
+
 if __name__ == "__main__":
     main()
-
-
-
-
-
-
-
-
+    
+    
+    
 
